c300a7af2b79b9ecb71cdcc0fabccaf4dff203ae
bhumi dolariya <dolariyabhumi29@gmail.com>
2026-04-20 18:59:41 +0530

    Day 3: Cards and Grid layout completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Card from './Components/Card';
 import Navbar from './Components/Navbar';
+import Projects from './Components/Projects';
 
 function App() {
   return (
     <>
    
     <Navbar/>
-    <Card/>
+    <Projects/>
+    
    
     </>
    
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './Card';
+
+
+function Projects() {
+  return (
+    <div className='bg-gray-100 min-h-screen p-6'>
+        <h1 className='text-3xl font-bold text-center mb-8'>My Projects</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            <Card title="project 1" desc="React + Tailwind project"/>
+            <Card title="Project 2" desc="API integration app" />
+            <Card title="Project 3" desc="Portfolio website" />
+            <Card title="Project 4" desc="Todo app" />
+            <Card title="Project 5" desc="Dashboard UI" />
+            <Card title="Project 6" desc="E-commerce UI" />
+        </div>
+    </div>
+  );
+}
+
+export default Projects;
